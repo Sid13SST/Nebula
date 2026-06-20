f464771c4f6e2cbb0f71983bb2d8ffa95f7cb85e
Siddhant <siddhant.prasad8@gmail.com>
2026-06-20 20:39:44 +0530

    feat: add sendKeys tool foundation

---
+++ src/tools/sendKeys.ts
@@ -0,0 +1,8 @@
+/**
+ * Tool to send keystrokes/text input to a targeted element on the page.
+ * @param selector CSS selector of the input field
+ * @param text The keys/text to send
+ */
+export async function sendKeys(selector: string, text: string): Promise<void> {
+  throw new Error('Not implemented yet');
+}
