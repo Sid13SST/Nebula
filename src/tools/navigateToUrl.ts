8b8509813c5f62e873e988ac2188d24961f5c2bc
Siddhant <siddhant.prasad8@gmail.com>
2026-06-20 20:39:43 +0530

    feat: add navigateToUrl tool foundation

---
+++ src/tools/navigateToUrl.ts
@@ -0,0 +1,7 @@
+/**
+ * Tool to navigate the active browser tab to a specified URL.
+ * @param url The destination URL
+ */
+export async function navigateToUrl(url: string): Promise<void> {
+  throw new Error('Not implemented yet');
+}
